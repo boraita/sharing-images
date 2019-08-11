affb5b0935f2af5b2b1875124a1fd49500e848b0
rmn <Rafael.MontanoBoraita@novatec-gmbh.de>
2019-08-11 17:39:55 +0200

    Change name Project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to sharingFotomatonSonrisas!');
+    expect(page.getTitleText()).toEqual('Welcome to sharingimages!');
   });
 
   afterEach(async () => {
